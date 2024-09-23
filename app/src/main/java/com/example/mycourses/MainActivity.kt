a9d78d0268b19218b02664f8ca4a41a01cc0e7f6
Pankaj <pankaj.awasthi@novoinvent.com>
2024-09-23 14:51:05 +0530

    About and details Screen

--- app/src/main/java/com/example/mycourses/MainActivity.kt
+++ app/src/main/java/com/example/mycourses/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mycourses
 
+import android.app.admin.SystemUpdateInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,9 +15,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -26,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,7 +47,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyCoursesTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    HomeScreen()
+//                    HomeScreen()
 
                 }
             }
@@ -118,5 +126,87 @@ fun CourseCard(item: Courses, onClick: () -> Unit) {
 
         }
 
+    }
+}
+
+@Composable
+fun AboutScreen(onNavigateUp: () -> Unit) {
+
+    Scaffold() { padding ->
+        Column(Modifier.padding(padding)) {
+            AppBar(title = "About", onNavigateUp)
+            Spacer(modifier = Modifier.height(20.dp))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = "This app is a demonstration about the navigation in" +
+                            "android jetpack compose"
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                val udemy_link = LocalUriHandler.current
+                Button(onClick = { udemy_link.openUri("https://www.udemy.com/course/mastering-jetpack-compose/learn/lecture/38145166#overview") }) {
+                    Text(text = "View Our Udemy ")
+                }
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun AppBar(title: String, onNavigateUp: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 10.dp)
+    ) {
+        IconButton(onClick = onNavigateUp) {
+            Icon(
+                Icons.Rounded.ArrowBack,
+                contentDescription = "Go Back"
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = title, fontSize = 24.sp)
+        }
+    }
+}
+
+@Composable
+fun DetailsScreen(title: String, name: String?, onNavigateUp: () -> Unit) {
+    val choosen_course = allCourses.first { it.title == title }
+
+    Scaffold() { padding ->
+        Column(Modifier.padding(padding)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(vertical = 10.dp)
+            ) {
+                IconButton(onClick = onNavigateUp) {
+                    Icon(
+                        Icons.Rounded.ArrowBack,
+                        contentDescription = "Go Back"
+                    )
+                }
+
+                Image(
+                    painterResource(id = choosen_course.thumbnail), contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16f / 9f),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Text(text = choosen_course.title, Modifier.fillMaxSize(), fontSize = 20.sp)
+
+                }
+
+            }
+        }
     }
 }
